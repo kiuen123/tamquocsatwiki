221be743412b544c7b35cddcd24915f0ab172400
TheIceElfKing <theiceelfking@gmail.com>
2022-05-26 21:09:36 +0700

    delete all

--- src/bin/www.js
+++
@@ -1,7 +0,0 @@
-const app = require("../apps/app");
-const config = require("config");
-
-const server = app.listen(port=config.get("app").port, () => {
-    console.log("Server started on port "+port);
-});
-require("../apps/chat")(server);
--- src/common/transporter.js
+++
@@ -1,6 +0,0 @@
-const nodemailer = require("nodemailer");
-const config = require("config");
-const mail = config.get("mail");
-const transporter = nodemailer.createTransport(mail);
-
-module.exports = transporter;
